b1478ae891bb26030e34239d8d4b6839b27d91b9
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-24 06:44:54 +0100

    create new table to handle comments

--- app/sql/Database.sql
+++ app/sql/Database.sql
@@ -16,3 +16,12 @@ CREATE TABLE IF NOT EXISTS user (
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS comment (
+    commentID INT(11) PRIMARY KEY AUTO_INCREMENT,
+    userID INT(11) NOT NULL ,
+    recipyID INT (11) NOT NULL ,
+    comment LONGTEXT NOT NULL,
+    FOREIGN KEY (userID) REFERENCES user(userID),
+    FOREIGN KEY (recipyID) REFERENCES recipy(recipyID)
+);
